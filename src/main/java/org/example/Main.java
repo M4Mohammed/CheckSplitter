633f6f2e7b11560de646d88e29615c4fdd484142
M4Mohammed <mohammedramadan2000@gmail.com>
2022-10-11 15:17:14 +0200

    cleaned driver code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,42 +13,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-//        Participant MH = new Participant("Mohamed");
-//        Participant MG = new Participant("Momen");
-//        Participant YG = new Participant("Youssef");
-//        CheckoutHandler checkoutHandler = new CheckoutHandler(new SharesHandler());
-//
-//        List<Participant> MMY = new ArrayList<>();
-//        MMY.add(MH);
-//        MMY.add(MG);
-//        MMY.add(YG);
-//
-//        List<Participant> MHY = new ArrayList<>();
-//        MHY.add(MH);
-//        MHY.add(YG);
-//
-//        List<Participant> MGY = new ArrayList<>();
-//        MGY.add(MG);
-//        MGY.add(YG);
-//
-//        List<Participant> MHMG = new ArrayList<>();
-//        MHMG.add(MH);
-//        MHMG.add(MG);
-//
-//
-//        ShoppingCart MyShoppingCart = new ShoppingCart(new ArrayList<>());
-//        MyShoppingCart.addItem(new Item(75, MMY));
-//        MyShoppingCart.addItem(new Item(100, MMY));
-//        MH.addItemToTab(6.5f);
-//        YG.addItemToTab(-37.95f);
-//
-//        checkoutHandler.calculateMoneyOwedByEachParticipant(MyShoppingCart);
-//
-//        System.out.println(MH.getTab());
-//        System.out.println(YG.getTab());
-//        System.out.println(MG.getTab());
 
-        CheckoutHandler checkoutHandler = new CheckoutHandler(new SharesHandler());
+     CheckoutHandler checkoutHandler = new CheckoutHandler(new SharesHandler());
         ShoppingCart MyShoppingCart = new ShoppingCart(new ArrayList<>());
 
         List<Participant> participants = InputHandler.createParticipantGroupPrompt();
